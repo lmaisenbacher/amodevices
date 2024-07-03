078364aa57ef1b1a37e46b45e99d5060f7e019bd
Lothar Maisenbacher <lothar.maisenbacher@mpq.mpg.de>
2024-07-03 15:01:08 -0700

    Bumped version to 0.1.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='amodevices',
-    version='0.1.1',
+    version='0.1.2',
     author='Lothar Maisenbacher',
     author_email='lothar.maisenbacher@berkeley.edu',
     description='Drivers for AMO (atomic, molecular, and optical physics) laboratory devices.',
