6ed207eb4fe4fa6c796ab71dbda8898547edcd02
Lothar Maisenbacher <lothar.maisenbacher@berkeley.edu>
2025-10-16 12:29:06 -0700

    Added AI back in.

--- amodevices/ni_daq/ni_daq.py
+++ amodevices/ni_daq/ni_daq.py
@@ -10,6 +10,7 @@ Device driver for NI DAQ devices using DAQmx interface.
 
 import logging
 import nidaqmx
+from nidaqmx.errors import DaqWriteError
 
 from .. import dev_generic
 from ..dev_exceptions import DeviceError
@@ -22,17 +23,34 @@ class NIDAQ(dev_generic.Device):
     """
 
     def __init__(self, config):
-        self.ao_channels = config['AOChannels']
+        self.ao_channel_default = config.get('AOChannelDefault', {})
+        self.ao_channels = {
+            axis: {**self.ao_channel_default, **chan}
+            for axis, chan in config.get('AOChannels', {}).items()}
         self.ao_tasks = {axis: nidaqmx.Task() for axis in self.ao_channels}
-        self.voltages = {axis: 0. for axis in self.ao_channels}
+        self.ao_voltages = {axis: None for axis in self.ao_channels}
+
+        self.ai_channel_default = config.get('AIChannelDefault', {})
+        self.ai_channels = {
+            axis: {**self.ai_channel_default, **chan}
+            for axis, chan in config.get('AIChannels', {}).items()}
+        self.ai_tasks = {axis: nidaqmx.Task() for axis in self.ai_channels}
+        self.ai_voltages = {axis: None for axis in self.ai_channels}
+
         self.initialized = False
 
     def connect(self):
         for axis, chan in self.ao_channels.items():
             self.ao_tasks[axis].ao_channels.add_ao_voltage_chan(
-                physical_channel=chan,
-                min_val=0.0,
-                max_val=10.0
+                physical_channel=chan['ChannelName'],
+                min_val=chan.get('MinVal', -10.),
+                max_val=chan.get('MaxVal', 10.)
+            )
+        for axis, chan in self.ai_channels.items():
+            self.ai_tasks[axis].ai_channels.add_ai_voltage_chan(
+                physical_channel=chan['ChannelName'],
+                min_val=chan.get('MinVal', -10.),
+                max_val=chan.get('MaxVal', 10.)
             )
         self.initialized = True
 
@@ -40,10 +58,23 @@ class NIDAQ(dev_generic.Device):
         for task in self.ao_tasks.values():
             task.stop()
             task.close()
+        for task in self.ai_tasks.values():
+            task.stop()
+            task.close()
         self.initialized = False
+
+    def read_voltage(self, axis):
+        if axis not in self.ai_channels:
+            raise DeviceError(f'Unknown AI axis: \'{axis}\'')
+        voltage = self.ai_tasks[axis].read()
+        self.ai_voltages[axis] = voltage
+        return voltage
 
     def set_voltage(self, axis, voltage):
         if axis not in self.ao_channels:
             raise DeviceError(f'Unknown AO axis: \'{axis}\'')
-        self.ao_tasks[axis].write(voltage)
-        self.voltages[axis] = voltage
+        try:
+            self.ao_tasks[axis].write(voltage)
+        except DaqWriteError as e:
+            raise DeviceError(e)
+        self.ao_voltages[axis] = voltage
--- amodevices/ni_daq/test.py
+++ amodevices/ni_daq/test.py
@@ -11,23 +11,49 @@ from amodevices.dev_exceptions import DeviceError
 
 # Configuration with your device names
 device = {
+    'AOChannelDefault': {
+        'MinVal': 0.,
+        'MaxVal': 10.,
+        },
     'AOChannels': {
-        'x': 'cDAQ1Mod2/ao8',
-        'y': 'cDAQ1Mod2/ao9',
-        'z': 'cDAQ1Mod2/ao10',
+        'x': {'ChannelName': 'cDAQ1Mod2/ao8',},
+        'y': {'ChannelName': 'cDAQ1Mod2/ao9',},
+        'z': {'ChannelName': 'cDAQ1Mod2/ao10',},
+        },
+    'AIChannelDefault': {
+        'MinVal': -10.,
+        'MaxVal': 10.,
+        },
+    'AIChannels': {
+        'x': {'ChannelName': 'cDAQ1Mod4/ai8',},
+        'y': {'ChannelName': 'cDAQ1Mod4/ai9',},
+        'z': {'ChannelName': 'cDAQ1Mod4/ai10',},
         }
 }
 
 daq = NIDAQ(device)
 
+axes = ['x', 'y', 'z']
+
 try:
     daq.connect()
     print("DAQ connected.")
 
+    # Read voltages on each AI channel
+    for axis in axes:
+        voltage = daq.read_voltage(axis)
+        print(f"Read AI voltage on '{axis}' as {voltage:.6f} V")
+
     # Set voltages on each AO channel
-    for axis, voltage in zip(['x', 'y', 'z', 'g'], [0.5, 2.5, 4.0, 0.0]):
+    for axis, voltage in zip(axes, [0.5, 1.0, 1.5]):
         daq.set_voltage(axis, voltage)
         print(f"Set AO voltage on '{axis}' to {voltage:.2f} V")
+
+    # Read voltages on each AI channel
+    for axis in axes:
+        voltage = daq.read_voltage(axis)
+        print(f"Read AI voltage on '{axis}' as {voltage:.6f} V")
+
 except DeviceError as e:
     print(e.value)
 finally:
